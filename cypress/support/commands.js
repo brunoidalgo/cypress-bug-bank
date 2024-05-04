468191c65d68d988bc80c9d9fd7ecb86992cb0bf
brunoidalgo <idalgob99@gmail.com>
2024-05-04 19:30:24 -0300

    Iniciando Projeto

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,4 +5,5 @@ Cypress.Commands.add("login", () => {
     cy.visit(pages.homepage)
     cy.xpath('(//input[@placeholder="Informe seu e-mail"])[1]').type(users.brunoempke.login)
     cy.xpath('(//input[@placeholder="Informe sua senha"])[1]').type(users.brunoempke.password)
+    cy.xpath('(//button[@type="submit"])[1]').click()
 })
